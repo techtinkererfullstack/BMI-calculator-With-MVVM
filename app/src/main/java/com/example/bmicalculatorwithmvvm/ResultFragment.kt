0ae59f8117882f4336add0ef9b3d7d5ba9c8497d
shafe <alam.shafee@gmail.com>
2026-02-19 22:46:51 +0600

    implemented live data

--- app/src/main/java/com/example/bmicalculatorwithmvvm/ResultFragment.kt
+++ app/src/main/java/com/example/bmicalculatorwithmvvm/ResultFragment.kt
@@ -22,9 +22,18 @@ class ResultFragment : Fragment() {
 //        no need to get bundle data after view model implementation
 //        val bmi = arguments?.getDouble("bmi")
 //        bmiScoreTV.text = bmi.toString()
-        binding.bmiResult.text =
-            String.format("%.1f", viewModel.bmi) // to show only 1 decimal after .
-        binding.categoryTV.text = viewModel.category
+
+//        setting observer
+        viewModel.bmi.observe(viewLifecycleOwner, { bmi ->
+            binding.bmiResult.text =
+                String.format("%.1f", bmi) // to show only 1 decimal after .
+        })
+
+//        setting observer
+        viewModel.category.observe(viewLifecycleOwner, {category ->
+            binding.categoryTV.text = category
+        })
+
         return binding.root
     }
 }
